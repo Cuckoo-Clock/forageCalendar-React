9a4ff7240dce8d4feeacb8a37591462e3759ecbb
AO <ao@elizabeerbookg4.myfiosgateway.com>
2021-01-07 20:16:27 -0500

    added another component

---
+++ src/components/Safepractices.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function WarningModal(){
+    return(
+        <>
+        {/*  <Modal> */} {/* whadda i need to import?*/}
+        <p>it's a warning modal</p>
+        {/*  </Modal> */}
+        </>
+    );
+}
+
+// onscroll in react?
+
+export default function Safepractices(){
+    return(
+        <>
+        <WarningModal/>
+        <h1>Some Thoughts on Safe and Sustainable Practices</h1>
+        </>
+    );
+}
