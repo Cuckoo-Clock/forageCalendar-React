21358cf2ce6b4b687714058aab02812a1352cd68
AO <ao@elizabeerbookg4.myfiosgateway.com>
2021-01-08 11:55:50 -0500

    flushing out page contents

--- src/components/FieldguideEntry.jsx
+++ src/components/FieldguideEntry.jsx
@@ -1,50 +1,59 @@
-import  React from "react";
+import React from "react";
 // import { mushrooms } from "../shared/mushroomIndex";
 
 // "I got props from the link, and that populates me as?
 
+function PhotoDisplay({ photo, photos }) {
+  const toggleThumb = () => {
+    console.log("i be big now");
+  };
+  const imgs = [...photos, photo]; //use this to map thumbnails.
+  const thumbs = imgs.map((image) => {
+    const url = image.split("/");
 
-function PhotoDisplay({photo, photos}){ {/* spread operator them together*/}
+    // a differnt option, better bc no unnecessary array
+    // const lastItem = thePath.substring(thePath.lastIndexOf('/') + 1)
+    // https://flaviocopes.com/how-to-get-last-item-path-javascript/
 
-    // a component to showcase grid thumbnails and have a enlarghed image
-    //circles growing up larger
-    // name over scrim on image?
-    //links again.
-    return(
-        <>
-        <p>pip</p>
-        </>
+    const imgName = url[url.length - 2]; //retrieves the directory name for the specimen, which is the speciment name
+    console.log(imgName);
+    // slice the name to get evrything from the end until the slash
+    return (
+      <a onClick={toggleThumb}>
+        <img src={image} alt={imgName} />
+      </a>
     );
+  });
+
+  //circles growing up larger
+  // name over scrim on image?
+  //links again.
+
+  return <div id="thumbs">{thumbs}</div>;
 }
 
+export default function FieldguideEntry({ specimen }) {
+  return (
+    <>
+      <h2>
+        <i>{specimen.binomial}</i>
+      </h2>
+      <h3>{`Common Names: ${specimen.names.join(", ")}`}</h3>
 
-export default function FieldguideEntry({specimen}){
-    return(
-            
-              <>
-              <h2><i>{specimen.binomial}</i></h2>
-              <h3>{`Common Names: ${specimen.names.join(", ")}`}</h3>
-              
-              <img src={specimen.photo} alt={`image of ${specimen.binomial}`}/>
-              <PhotoDisplay photo={specimen.photo} photos={specimen.photos}/>
-              <h3>Charactersitics</h3>
-              <p>{specimen.description.characteristics}</p>
-              <p>{specimen.description.habitat}</p>
-              {/* conditional for danger flag 
+      {/* <img src={specimen.photo} alt={`image of ${specimen.binomial}`} /> */}
+      <PhotoDisplay photo={specimen.photo} photos={specimen.photos} />
+      <h3>Charactersitics</h3>
+      <p>{specimen.description.characteristics}</p>
+      <p>{specimen.description.habitat}</p>
+      {/* conditional for danger flag 
               
               */}
-              <h3>Look Alikes</h3>
-              {/* <PhotoDisplay photos={specimen.lookAlikes}/> */}
-              <h3>Edibility: {specimen.edibility}</h3>
-              <p>{specimen.cooking.notes}</p>
-              <h4>Recipes</h4>
-              <ul>
-                  {/* map the recipe links? */}
-              </ul>
-
-              
-
-              </>
-          
-    );
+      <h3>Look Alikes</h3>
+      {/* <PhotoDisplay photos={specimen.lookAlikes}/> */}
+      <h3>Edibility: {specimen.edibility}</h3>
+      <p>{specimen.cooking.notes}</p>
+      <h4>Recipes</h4>
+      <ul>{/* map the recipe links? */}</ul>
+    </>
+  );
 }
---
+++ src/shared/safePractices_content.js
@@ -0,0 +1,16 @@
+export const pageContent = [
+  {
+    heading: "Safe & Sustainable practices",
+    content:
+      "Officia cupidatat mollit sint exercitation excepteur esse magna. Enim cillum cillum ad deserunt anim duis Lorem excepteur quis ut laboris ipsum exercitation. Consequat proident est cillum ut do incididunt nulla ut fugiat duis velit deserunt enim laboris. Ipsum eu labore pariatur occaecat.\nId sit aliquip laborum veniam exercitation minim qui cillum commodo id non. Id non consequat eu cupidatat consequat nulla eu non commodo proident quis deserunt irure consequat. Aute consectetur adipisicing id nostrud et incididunt duis reprehenderit proident consectetur duis dolor.\n Lorem quis aliquip labore ea nulla eu. Proident pariatur id in culpa occaecat elit non do ipsum nisi dolore aute adipisicing est. Cupidatat eiusmod aute commodo id. Ipsum deserunt dolore consequat tempor officia sint dolore aute duis minim commodo officia proident id. Aute tempor veniam pariatur adipisicing eiusmod non in nulla Lorem laboris deserunt voluptate. Ad non sit adipisicing eiusmod consequat do ad magna quis eu. \n Non dolore aute quis esse labore. Consectetur enim do ex sunt nostrud labore adipisicing nisi consequat sit nisi est deserunt. Ipsum dolor sunt ex aliquip eiusmod non laboris amet laborum. Cupidatat mollit exercitation eiusmod est elit deserunt veniam consectetur magna non mollit.",
+  },
+];
+
+export const warnings = [
+  {
+    heading: "Eating unknown mushrooms can be fatal",
+    content:
+      "be thorough. Ut Lorem velit magna incididunt dolore.Et commodo do laborum mollit in ullamco.Aliqua aliqua laborum enim nostrud non consectetur irure consequat tempor velit proident aliqua exercitation.",
+  },
+  { heading: "Warning", content: "really, don't use your belly" },
+];
